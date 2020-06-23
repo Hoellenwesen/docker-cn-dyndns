3760b956dc9f06a307200d653dd0398c875cce47
Sebastian Weyer <Hoellenwesen@users.noreply.github.com>
2020-06-23 10:40:24 +0200

    Next Test

--- entry.sh
+++ entry.sh
@@ -19,14 +19,14 @@ then
 fi
 
 # Add credentials to DynDNS Changer config
-sed -i "s/{{DNS_API_USER}}/$DNS_API_USER/g" /dyndns/config.ini
-sed -i "s/{{DNS_API_PASS}}/$DNS_API_PASS/g" /dyndns/config.ini
-sed -i "s/{{DNS_ZONE}}/$DNS_ZONE/g" /dyndns/config.ini
-sed -i "s/{{DNS_SUB1}}/$DNS_SUB1/g" /dyndns/config.ini
-sed -i "s/{{DNS_SUB2}}/$DNS_SUB2/g" /dyndns/config.ini
-sed -i "s/{{DNS_SUB3}}/$DNS_SUB3/g" /dyndns/config.ini
-sed -i "s/{{DNS_SUB4}}/$DNS_SUB3/g" /dyndns/config.ini
-sed -i "s/{{DNS_CRONTAB}}/$DNS_CRONTAB/g" /crontab.txt
+#sed -i "s/{{DNS_API_USER}}/$DNS_API_USER/g" /dyndns/config.ini
+#sed -i "s/{{DNS_API_PASS}}/$DNS_API_PASS/g" /dyndns/config.ini
+#sed -i "s/{{DNS_ZONE}}/$DNS_ZONE/g" /dyndns/config.ini
+#sed -i "s/{{DNS_SUB1}}/$DNS_SUB1/g" /dyndns/config.ini
+#sed -i "s/{{DNS_SUB2}}/$DNS_SUB2/g" /dyndns/config.ini
+#sed -i "s/{{DNS_SUB3}}/$DNS_SUB3/g" /dyndns/config.ini
+#sed -i "s/{{DNS_SUB4}}/$DNS_SUB4/g" /dyndns/config.ini
+#sed -i "s/{{DNS_CRONTAB}}/$DNS_CRONTAB/g" /crontab.txt
 
 # Start cron
 /usr/sbin/crond -f -l 8
